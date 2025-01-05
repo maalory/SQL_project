b8b53e82f504976ee457b435c8f220bb02917014
maalory <168477148+maalory@users.noreply.github.com>
2025-01-05 16:54:16 +0100

    Add files via upload

---
+++ SQL_projekt_Balak.sql
@@ -0,0 +1,19 @@
+--- TABULKA 1 DONE - KOMODITY - PricesByYear ... year, code, name, avg_price
+    
+SELECT
+    	date_format(cp.date_from, '%Y') as year,
+		cp.category_code,
+		cpc.name,
+		cpc.price_value as unit_value,
+		cpc.price_unit,
+		round(AVG(cp.value),2) as avg_price_value
+    FROM 
+        czechia_price cp
+    JOIN czechia_price_category cpc 
+    	on cpc.code = cp.category_code
+    GROUP BY 
+   		date_format(cp.date_from, '%Y'), 
+    	cp.category_code, 
+    	cpc.name, 
+   		cpc.price_value, 
+   		cpc.price_unit;
