1e9a7c9c96f0b9423d124e07f02e5844ab4835d6
Tomáš Balák <tomasbalak@gmail.com>
2025-01-05 21:23:53 +0100

    created wage comparison

--- ukol1.sql
+++ ukol1.sql
@@ -1,9 +1,6 @@
 -- UKOL 1: rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
-SELECT *
-FROM PrimaryFinal
-limit 5;
--- TAB 1 ...wage trend + comparison
-
+-- TAB 1 DONE ...wage trend + comparison
+WITH wage_trends AS(
    SELECT
        year,
        industry_name,
@@ -11,4 +8,20 @@ limit 5;
        LAG(total_gross_wage) OVER (PARTITION BY industry_name ORDER BY year) AS prev_gross_wage
    FROM PrimaryFinal
    WHERE total_gross_wage IS NOT NULL
-     AND industry_name IS NOT NULL;
+     AND industry_name IS NOT NULL
+     ),
+-- TAB 2 srovnani mezd
+wage_comparison AS (
+   SELECT
+       industry_name,
+       year,
+       total_gross_wage,
+       prev_gross_wage,
+       CASE
+           WHEN prev_gross_wage IS NOT NULL AND total_gross_wage < prev_gross_wage THEN 1
+           ELSE 0
+       END AS wage_decline_flag
+   FROM wage_trends
+)
+SELECT *
+FROM wage_comparison;
