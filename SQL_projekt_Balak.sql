eef485cbd699c6fe45e8da74dd8d79f1854b259e
Tomáš Balák <tomasbalak@gmail.com>
2025-01-05 19:46:01 +0100

    added cz_payroll tab

--- SQL_projekt_Balak.sql
+++ SQL_projekt_Balak.sql
@@ -18,4 +18,18 @@ SELECT
    		cpc.price_value, 
    		cpc.price_unit;
    		
- -- TEST
+-- TABULKA 2 DONE - CZ_PAYROLL - year, industry_branch_code, branch -> cz_payroll_industry_branch, filtr: NULL, value_type_code = 5958 = mzda
+   	
+SELECT
+	pay.payroll_year,
+	pay.industry_branch_code,
+	cpib.name,
+	sum(pay.value) as total_gross_wage
+FROM czechia_payroll pay
+JOIN czechia_payroll_industry_branch cpib
+	ON cpib.code = pay.industry_branch_code
+WHERE pay.value is not null
+AND pay.industry_branch_code is not null
+AND pay.value_type_code = 5958
+AND pay.calculation_code = 200
+GROUP BY pay.payroll_year, pay.industry_branch_code, cpib.name;
