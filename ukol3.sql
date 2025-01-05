165a603f5b3a35ae925b8f2f967aba47f1619c8a
Tomáš Balák <tomasbalak@gmail.com>
2025-01-05 22:23:56 +0100

    Task3 - created tables: prices trends, %growth, growth by cat

---
+++ ukol3.sql
@@ -0,0 +1,41 @@
+-- UKOL 3: Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
+SELECT *
+FROM PrimaryFinal
+limit 5;
+-- TAB 1 DONE ...trend cen potravin - year, name, price, previous year price
+	SELECT
+       year,
+       category_name,
+       avg_price_value,
+       LAG(avg_price_value) OVER (PARTITION BY category_name ORDER BY year) AS prev_avg_price_value
+   FROM PrimaryFinal
+  	WHERE category_code IS NOT NULL;
+
+  -- TAB 2... %growth
+  	
+	SELECT
+       year,
+       category_name,
+       avg_price_value,
+       prev_avg_price_value,
+       CASE
+           WHEN prev_avg_price_value IS NOT NULL THEN
+               ROUND(((avg_price_value - prev_avg_price_value) / prev_avg_price_value) * 100, 2)
+           ELSE NULL
+       END AS percent_change
+   FROM price_growth
+   );
+  
+-- TAB 3...growth by cat
+
+   SELECT
+       category_name,
+       ROUND(AVG(percent_change), 2) AS avg_percent_growth
+   FROM percent_growth
+   WHERE percent_change IS NOT NULL
+   GROUP BY category_name
+)
+SELECT
+   category_name,
+   avg_percent_growth
+FROM avg_growth_by_category;
