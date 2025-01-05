7ce3b2b761d8e0ace94ea6a945fd2dc69b025a86
Tomáš Balák <tomasbalak@gmail.com>
2025-01-05 22:17:26 +0100

    completed SELECT using WITH

--- ukol2.sql
+++ ukol2.sql
@@ -1,65 +1,61 @@
 -- UKOL 2. Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
-
-SELECT
-   year,
-   sum(total_gross_wage)
-FROM PrimaryFinal
-group by year;
-
-SELECT
-   min(year),
-   category_name,
-   avg_price_value,
-   price_unit
-FROM PrimaryFinal
-WHERE category_name = 'Mléko polotučné pasterované';
-
--- !!!OK mleko_cena_y: roky + cena mleka
+ WITH mleko_cena AS(
+	SELECT
+       year,
+       category_name,
+       avg_price_value as avg_price_value_milk,
+       price_unit
+   FROM PrimaryFinal
+   WHERE category_name = 'Mléko polotučné pasterované'
+   ),
+   chleb_cena AS(
    SELECT
        year,
        category_name,
-       avg_price_value,
+       avg_price_value as avg_price_value_bread,
        price_unit
    FROM PrimaryFinal
-   WHERE category_name = 'Mléko polotučné pasterované';
-  
--- !!!OK chleba_cena_y: roky + cena mleka
-   SELECT
-       year,
-       category_name,
-       avg_price_value,
-       price_unit
-   FROM PrimaryFinal
-   WHERE category_name = 'Chléb konzumní kmínový';
-  
- -- !!!mzdy_y
-   SELECT
+   WHERE category_name = 'Chléb konzumní kmínový'
+   ),
+   mzdy_y AS(
+  	SELECT
        year,
        ROUND(AVG(total_gross_wage), 2) AS total_avg_gross_wage
    FROM PrimaryFinal
    WHERE total_gross_wage IS NOT NULL
-   GROUP BY year;
- 
--- min rok mzdy (2000) - nepoužívat
-  select
-  		min(year) as min_rok_mzdy
-  from PrimaryFinal
-  where total_gross_wage is not null;
- 
--- max rok mzdy (2021) - nepoužívat
-  select
-  		max(year) as max_rok_mzdy
-  from PrimaryFinal
-  where total_gross_wage is not null;
- 
- -- !!!min rok ceny (2006)
-  select
+   GROUP BY year
+   ),
+	cat_min_rok AS(
+   select
   		min(year) as min_rok
-  from PrimaryFinal
-  where category_name is not null; 
- 
--- !!!max rok ceny (2018)
-  select
+   from PrimaryFinal
+   where category_name is not null
+   ),
+   cat_max_rok AS (
+   select
   		max(year) as max_rok
-  from PrimaryFinal
-  where category_name is not null;
+   from PrimaryFinal
+   where category_name is not null
+   ),
+	combined_data AS (
+   SELECT
+       mzdy_y.year,
+       mzdy_y.total_avg_gross_wage,
+       mleko_cena.avg_price_value_milk,
+       chleb_cena.avg_price_value_bread
+   FROM mzdy_y
+   JOIN mleko_cena
+       ON mzdy_y.year = mleko_cena.year
+   JOIN chleb_cena
+       ON mzdy_y.year = chleb_cena.year
+)
+SELECT
+   CASE
+       WHEN cd.year = (SELECT min_rok FROM cat_min_rok) THEN 'Prvni rok'
+       WHEN cd.year = (SELECT max_rok FROM cat_max_rok) THEN 'Posledni rok'
+   END AS period,
+   cd.year,
+   ROUND(cd.total_avg_gross_wage / cd.avg_price_value_milk, 2) AS za_prumernou_mzdu_v_roce_litru_mleka,
+   ROUND(cd.total_avg_gross_wage / cd.avg_price_value_bread, 2) AS za_prumernou_mzdu_v_roce_kg_chleba
+FROM combined_data cd
+WHERE cd.year IN ((SELECT min_rok FROM cat_min_rok), (SELECT max_rok FROM cat_max_rok));
